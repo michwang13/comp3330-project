c42589c8de74a7036c7417f974d3d0fbff7635f0
michael <marcell.michael27@gmail.com>
2022-11-10 20:40:49 +0800

    Implemented edit photo in ManualInputActivity

--- app/src/main/java/hk/hkucs/comp3330_project/Item.kt
+++ app/src/main/java/hk/hkucs/comp3330_project/Item.kt
@@ -1,11 +1,12 @@
 package hk.hkucs.comp3330_project
 
-class Item(itemName: String?, notes: String?, category: String?, expiryDate: String?, reminder: String?) {
+class Item(itemName: String?, notes: String?, category: String?, expiryDate: String?, reminder: String?, imageURI: String?) {
     var itemName: String
     var notes: String
     var category: String
     var expiryDate: String
     var reminder: String
+    var imageURI: String
 
     init{
         this.itemName = itemName ?: ""
@@ -13,6 +14,7 @@ class Item(itemName: String?, notes: String?, category: String?, expiryDate: Str
         this.category = category ?: ""
         this.expiryDate = expiryDate ?: ""
         this.reminder = reminder ?: ""
+        this.imageURI = imageURI ?: ""
     }
 
 
